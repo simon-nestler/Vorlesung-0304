061047cdb5101de665113a0db5d1a54d7619076c
Simon Nestler <simon.nestler@thi.de>
2025-04-03 11:51:01 +0200

    gui und systemin

--- src/KeyboardInput.java
+++ src/KeyboardInput.java
@@ -2,12 +2,22 @@ import java.io.IOException;
 
 public class KeyboardInput {
 
-    public KeyboardInput() throws IOException {
+    public KeyboardInput() {
+
+    }
+
+    public void viaSystemIn() throws IOException {
         int i = 100;
 
         while (i > 31) {
             i = System.in.read();
             System.out.print((char) i);
         }
+    }
+
+    public void viaGUI() {
+        String s = javax.swing.JOptionPane.showInputDialog("Enter a number");
+        int i = Integer.parseInt(s);
+        System.out.println(i);
     }
 }
