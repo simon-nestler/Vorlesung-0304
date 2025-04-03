128467b608f16cfcd67f7c53e190a45949a0939b
Simon Nestler <simon.nestler@thi.de>
2025-04-03 12:04:20 +0200

    bufferedreader

--- src/KeyboardInput.java
+++ src/KeyboardInput.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
 
 public class KeyboardInput {
 
@@ -19,5 +22,31 @@ public class KeyboardInput {
         String s = javax.swing.JOptionPane.showInputDialog("Enter a number");
         int i = Integer.parseInt(s);
         System.out.println(i);
+    }
+
+    public void viaBuffer() {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        try {
+            System.out.println("Gebe Sie Ihren Namen ein:");
+            String name = br.readLine();
+            System.out.println("Gebe Sie Ihr Alter ein:");
+            int alter = 0;
+
+            while (alter == 0) {
+                try {
+                    alter = Integer.parseInt(br.readLine());
+                } catch (NumberFormatException e) {
+                    alter = 0;
+                    System.out.println("Bitte geben Sie eine gültige Zahl ein.");
+                }
+            }
+
+            System.out.println("Hallo " + name + ", Sie sind " + alter + " Jahre alt.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
